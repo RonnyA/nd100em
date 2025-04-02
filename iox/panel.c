8fb96579bed0915b28d2400496edad61dc2f52d2
Ronny Hansen <ronnyha@gmail.com>
2025-04-02 16:12:17 +0200

    Enhanced panel functionality by implementing message control processing, updating machine time calculations, and refining the setup process. Added new enums for message control commands and improved structure definitions for better clarity and functionality.

--- iox/panel.c
+++ iox/panel.c
@@ -1,163 +1,433 @@
 #include "panel.h"
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "../nd100.h"
 
-void setup_pap(){
-    
-//	gPANS=0x8000;	/* Tell system we are here */
-	//gPANS=gPANS | 0x4000;	/* Set FULL which is active low, so not full */
-    
-	gPAP = calloc(1,sizeof(struct display_panel));
+void setup_pap()
+{
+
+	gPANS = 0x8000;			/* Tell system we are here */
+	gPANS = gPANS | 0x4000; /* Set FULL which is active low, so not full */
+
+	gPAP = calloc(1, sizeof(struct display_panel));
+
+	UpdateMachineTime();
 }
 
+void ProcessMessageControl(PANC_Register panc)
+{
+	MessageControl mc = (panc.bits.wpan & 0b111);
+
+	// if (debug) fprintf(debugfile, "Processing Message Control Command: %d WPAN=%d\n", mc, WPAN);
+
+	switch (mc)
+	{
+	case StopRotatingMessage:
+		gPAP->function_mode = 0; // Stop rotating
+		break;
+	case ReturnDisplayToNormal:
+		gPAP->function_mode = 1; // Return to normal
+		break;
+	case ClearTextBuffer:
+		gPAP->fdisp_cntr = 0;									   // Clear display counter
+		memset(gPAP->func_display, 0, sizeof(gPAP->func_display)); // Clear display buffer
+		break;
+	case RotateMessage:
+		gPAP->function_mode = 2; // Start rotating
+		break;
+	case ClearAndRotate:
+		gPAP->fdisp_cntr = 0;									   // Clear display counter
+		memset(gPAP->func_display, 0, sizeof(gPAP->func_display)); // Clear display buffer
+		gPAP->function_mode = 2;								   // Start rotating
+		break;
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// Called from TRR logic when "TRR PANC" has been executed
+/// Process the command in gPANC
+/// </summary>
+void ProcessTerminalPanc()
+{
+	if (!gPAP)
+		return;
+
+	PANC_Register panc;
+	panc.raw = gPANC;
+
+	PANS_Register pans;
+	pans.raw = 0;
+
+	// Update time from the Host realtime clock when reading
+	if (panc.bits.read_request)
+		UpdateMachineTime();
+
+	// Is this a system request?
+	if (panc.bits.pfunc)
+	{
+
+		pans.bits.panel_present = 1; // yes, we are here
+		pans.bits.input_pending = 1; // FIFO not full
+		pans.bits.pan_interrupt = 1; // Yes, we have a PAN interrupt
+
+		pans.bits.rpan = panc.bits.read_request; // Answering read or write request?
+		pans.bits.pfunc = panc.bits.pfunc;		 // whhat function are we responding to?
+
+		switch (panc.bits.pfunc)
+		{
+		case STATUS_ILLEGAL:
+			// TODO: Implement!
+			break;
+		case STATUS_FUTURE_EXTENSION:
+			// TODO: Implement!
+			break;
+		case STATUS_MESSAGE_APPEND:
+			if (panc.bits.read_request)
+			{
+				// Read request not implemented for message append
+			}
+			else
+			{
+				if (panc.bits.wpan != 0)
+				{
+					if (panc.bits.wpan == 0x0E)
+					{
+						// "Shift Out" character - switch to alternate character set??
+						// Not implemented yet
+					}
+					else
+					{
+						char c = (char)panc.bits.wpan;
+
+						// Check if we have room in the buffer
+						if (gPAP->fdisp_cntr < sizeof(gPAP->func_display) - 1)
+						{
+							gPAP->func_display[gPAP->fdisp_cntr++] = c;
+						}
+					}
+				}
+			}
+			break;
+		case STATUS_MESSAGE_CONTROL:
+			if (panc.bits.read_request)
+			{
+				// read what ?
+			}
+			else
+			{
+				ProcessMessageControl(panc);
+			}
+			break;
+		case STATUS_UPDATE_LOW_SECONDS:
+			if (panc.bits.read_request)
+			{
+				pans.bits.rpan = (ushort)(gPAP->seconds & 0xFF);
+				pans.bits.read_panel_valid = 1; // Yes, we have a valid response
+			}
+			else
+			{
+				gPAP->seconds = (ushort)((gPAP->seconds & 0xff00) | panc.bits.wpan);
+			}
+			break;
+		case STATUS_UPDATE_HIGH_SECONDS:
+			if (panc.bits.read_request)
+			{
+				pans.bits.rpan = (ushort)(gPAP->seconds >> 8 & 0xFF);
+				pans.bits.read_panel_valid = 1; // Yes, we have a valid response
+			}
+			else
+			{
+				gPAP->seconds = gPAP->seconds & 0x00ff | (panc.bits.wpan << 8);
+			}
+			break;
+		case STATUS_UPDATE_LOW_DAYS:
+			if (panc.bits.read_request)
+			{
+				pans.bits.rpan = (ushort)(gPAP->days & 0x00ff);
+				pans.bits.read_panel_valid = 1; // Yes, we have a valid response
+			}
+			else
+			{
+				gPAP->days = (gPAP->days & 0xFF00) | panc.bits.wpan;
+			}
+			break;
+		case STATUS_UPDATE_HIGH_DAYS:
+			if (panc.bits.read_request)
+			{
+				pans.bits.rpan = (ushort)(gPAP->days >> 8 & 0x00ff);
+				pans.bits.read_panel_valid = 1; // Yes, we have a valid response
+			}
+			else
+			{
+				gPAP->days = (gPAP->days & 0x00FF) | (ushort)(panc.bits.wpan << 8);
+			}
+			break;
+		case STATUS_MEMORY_EXAMINE:
+			// TODO: Implement!
+			break;
+		case STATUS_DATA_TO_EXAMINE:
+			// TODO: Implement!			
+			break;
+		case STATUS_ACTIVE_LEVELS:
+			// TODO: Implement!
+			break;
+		case STATUS_OUT_EXAMINE_MODE:
+			// TODO: Implement!
+			break;
+		case STATUS_SEND_LABEL:
+			// TODO: Implement!
+			break;
+		case STATUS_F_TYPED:
+			// TODO: Implement!
+			break;
+		}
+	}
+	gPANS = pans.raw;
+}
+
+// respond to TRR LMP
+void ProcessTerminalLamp()
+{
+	// read gLMP
+	gPANS = 0x0000;
+}
+/// <summary>
+/// Calculate HW clock info
+///
+/// HW Clock contains an offset since 00:00:00 1.January 1979 (TBASE)
+/// The clock counts seconds and half-days (12 hours) from this time.
+/// </summary>
+void UpdateMachineTime()
+{
+	time_t tbase = 283996800; // 1979-01-01 00:00:00 UTC
+	time_t now = time(NULL);
+
+	// Sintran doesn't support Y2K (without patches) so stay in year before 2000...
+	// Subtract 28 years from current time (2023-28 = 1995)
+	struct tm *tm_now = localtime(&now);
+	tm_now->tm_year -= 28;
+	now = mktime(tm_now);
+
+	// Get midnight of current day
+	struct tm *tm_midnight = localtime(&now);
+	tm_midnight->tm_hour = 0;
+	tm_midnight->tm_min = 0;
+	tm_midnight->tm_sec = 0;
+	time_t midnight = mktime(tm_midnight);
+
+	// Calculate days difference from TBASE
+	double days_diff = difftime(now, tbase) / (24.0 * 3600.0);
+	gPAP->days = (uint16_t)(days_diff * 2); // Convert to half-days
+
+	// Check if we've passed noon
+	if (tm_now->tm_hour >= 12)
+	{
+		gPAP->days++; // Add another half day
+		// Adjust time to count seconds since midnight
+		now = now - (12 * 3600);
+	}
+
+	// Calculate seconds since midnight
+	gPAP->seconds = (uint16_t)difftime(now, midnight);
+}
 
 #if _later_
-void panel_thread() {
+void panel_thread()
+{
 	int s;
 	int sock, connected, bytes_recieved;
 	char recv_data[1024];
 	struct sockaddr_in client_addr;
 	socklen_t sin_size;
 
-	if (debug) fprintf(debugfile,"(#)panel_thread running...\n");
-	if (debug) fflush(debugfile);
+	if (debug)
+		fprintf(debugfile, "(#)panel_thread running...\n");
+	if (debug)
+		fflush(debugfile);
 
 	do_listen(5000, 1, &sock);
-	if (debug) fprintf(debugfile,"\n(#)TCPServer Waiting for client on port 5000\n");
-	if (debug) fflush(debugfile);
+	if (debug)
+		fprintf(debugfile, "\n(#)TCPServer Waiting for client on port 5000\n");
+	if (debug)
+		fflush(debugfile);
 
-	while(CurrentCPURunMode != SHUTDOWN) {
-		sin_size = (socklen_t) sizeof(struct sockaddr_in);
-		connected = accept(sock, (struct sockaddr *)&client_addr,&sin_size);
-		if (debug) fprintf(debugfile,"(#)I got a panel connection from (%s , %d)\n",
-			inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
-		if (debug) fflush(debugfile);
-		while(CurrentCPURunMode != SHUTDOWN) {
-			bytes_recieved = recv(connected,recv_data,1024,0);
+	while (CurrentCPURunMode != SHUTDOWN)
+	{
+		sin_size = (socklen_t)sizeof(struct sockaddr_in);
+		connected = accept(sock, (struct sockaddr *)&client_addr, &sin_size);
+		if (debug)
+			fprintf(debugfile, "(#)I got a panel connection from (%s , %d)\n",
+					inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		if (debug)
+			fflush(debugfile);
+		while (CurrentCPURunMode != SHUTDOWN)
+		{
+			bytes_recieved = recv(connected, recv_data, 1024, 0);
 			recv_data[bytes_recieved] = '\0';
-			if (debug) fprintf(debugfile,"(#)PANEL DATA received\n");
-			if(strncmp("OPCOM_PRESSED\n",recv_data,strlen("OPCOM_PRESSED"))==0){
-				MODE_OPCOM=1;
-				if (debug) fprintf(debugfile,"(#)OPCOM_PRESSED\n");
-
-			} else if(strncmp("MCL_PRESSED\n",recv_data,strlen("MCL_PRESSED"))==0){
-				if (debug) fprintf(debugfile,"(#)MCL_PRESSED\n");
+			if (debug)
+				fprintf(debugfile, "(#)PANEL DATA received\n");
+			if (strncmp("OPCOM_PRESSED\n", recv_data, strlen("OPCOM_PRESSED")) == 0)
+			{
+				MODE_OPCOM = 1;
+				if (debug)
+					fprintf(debugfile, "(#)OPCOM_PRESSED\n");
+			}
+			else if (strncmp("MCL_PRESSED\n", recv_data, strlen("MCL_PRESSED")) == 0)
+			{
+				if (debug)
+					fprintf(debugfile, "(#)MCL_PRESSED\n");
 				/* TODO:: this should be in a separate routine DoMCL later */
 				CurrentCPURunMode = STOP;
 				/* NOTE:: buggy in that we cannot do STOP and MCL without a running cpu between.. FIXME */
 				while ((s = sem_wait(&sem_stop)) == -1 && errno == EINTR) /* wait for stop lock to be free and take it */
-					continue; /* Restart if interrupted by handler */
-				bzero(gReg,sizeof(struct CpuRegs));	/* clear cpu */
-				setbit(_STS,_O,1);
-				setbit_STS_MSB(_N100,1);
-				gCSR = 1<<2;    /* this bit sets the cache as not available */
-
-			} else if(strncmp("LOAD_PRESSED\n",recv_data,strlen("LOAD_PRESSED"))==0){
-				if (debug) fprintf(debugfile,"(#)LOAD_PRESSED\n");
-				gPC=STARTADDR;
+					continue;											  /* Restart if interrupted by handler */
+				bzero(gReg, sizeof(struct CpuRegs));					  /* clear cpu */
+				setbit(_STS, _O, 1);
+				setbit_STS_MSB(_N100, 1);
+				gCSR = 1 << 2; /* this bit sets the cache as not available */
+			}
+			else if (strncmp("LOAD_PRESSED\n", recv_data, strlen("LOAD_PRESSED")) == 0)
+			{
+				if (debug)
+					fprintf(debugfile, "(#)LOAD_PRESSED\n");
+				gPC = STARTADDR;
 				CurrentCPURunMode = RUN;
-				if (sem_post(&sem_run) == -1) { /* release run lock */
-					if (debug) fprintf(debugfile,"ERROR!!! sem_post failure panel_thread\n");
+				if (sem_post(&sem_run) == -1)
+				{ /* release run lock */
+					if (debug)
+						fprintf(debugfile, "ERROR!!! sem_post failure panel_thread\n");
 					CurrentCPURunMode = SHUTDOWN;
 				}
-			} else if(strncmp("STOP_PRESSED\n",recv_data,strlen("STOP_PRESSED"))==0){
-				if (debug) fprintf(debugfile,"(#)STOP_PRESSED\n");
+			}
+			else if (strncmp("STOP_PRESSED\n", recv_data, strlen("STOP_PRESSED")) == 0)
+			{
+				if (debug)
+					fprintf(debugfile, "(#)STOP_PRESSED\n");
 				CurrentCPURunMode = STOP;
 				/* NOTE:: buggy in that we cannot do STOP and MCL without a running cpu between.. FIXME */
 				while ((s = sem_wait(&sem_stop)) == -1 && errno == EINTR) /* wait for stop lock to be free and take it */
-					continue; /* Restart if interrupted by handler */
-			} else {
-				if (debug) fprintf(debugfile,"(#)Panel received:%s\n",recv_data);
+					continue;											  /* Restart if interrupted by handler */
 			}
-			if (debug) fflush(debugfile);
+			else
+			{
+				if (debug)
+					fprintf(debugfile, "(#)Panel received:%s\n", recv_data);
+			}
+			if (debug)
+				fflush(debugfile);
 		}
 	}
 	close(sock);
 	return;
 }
 
-
-void panel_event(){
+void panel_event()
+{
 	char tmpbyte;
 
-	if (gPAP->trr_panc) {	/* TRR has been issued, process command */
-		if (debug) fprintf(debugfile,"panel_event: TRR\n");
-		if (debug) fflush(debugfile);
+	if (gPAP->trr_panc)
+	{ /* TRR has been issued, process command */
+		if (debug)
+			fprintf(debugfile, "panel_event: TRR\n");
+		if (debug)
+			fflush(debugfile);
 		gPAP->trr_panc = false;
-		switch ((gPANC & 0x0700)>>8) {
-		case 0:		/* Illegal */
+		switch ((gPANC & 0x0700) >> 8)
+		{
+		case 0: /* Illegal */
 			break;
-		case 1:		/* Future extension */
+		case 1: /* Future extension */
 			break;
-		case 2:		/* Message Append */	// TODO: Not Implemented yet except basic return info
+		case 2: /* Message Append */ // TODO: Not Implemented yet except basic return info
 			gPANS = 0xd200;
 			break;
-		case 3:		/* Message Control */	// TODO: Not Implemented yet except basic return info
+		case 3: /* Message Control */ // TODO: Not Implemented yet except basic return info
 			gPANS = 0xd300;
 			break;
-		case 4:		/* Update Low Seconds */
-			if (gPANC & 0x2000){	/* Read */
+		case 4: /* Update Low Seconds */
+			if (gPANC & 0x2000)
+			{ /* Read */
 				tmpbyte = (gPAP->seconds) & 0x00ff;
 				gPANS = 0xf400 | tmpbyte;
-			} else {		/*Write */
+			}
+			else
+			{ /*Write */
 				tmpbyte = gPANC & 0x00ff;
 				gPAP->seconds = (gPAP->seconds & 0xff00) | tmpbyte;
 				gPANS = 0xd400;
 			}
 			break;
-		case 5:		/* Update High Seconds */
-			if (gPANC & 0x2000){	/* Read */
+		case 5: /* Update High Seconds */
+			if (gPANC & 0x2000)
+			{ /* Read */
 				tmpbyte = (gPAP->seconds >> 8);
 				gPANS = 0xf500 | tmpbyte;
-			} else {		/*Write */
+			}
+			else
+			{ /*Write */
 				tmpbyte = gPANC & 0x00ff;
-				gPAP->seconds = (gPAP->seconds & 0x00ff) | ((ushort)tmpbyte)<<8;
+				gPAP->seconds = (gPAP->seconds & 0x00ff) | ((ushort)tmpbyte) << 8;
 				gPANS = 0xd500;
 			}
 			break;
-		case 6:		/* Update Low Days */
-			if (gPANC & 0x2000){	/* Read */
+		case 6: /* Update Low Days */
+			if (gPANC & 0x2000)
+			{ /* Read */
 				tmpbyte = (gPAP->days) & 0x00ff;
 				gPANS = 0xf600 | tmpbyte;
-			} else {		/*Write */
+			}
+			else
+			{ /*Write */
 				tmpbyte = gPANC & 0x00ff;
 				gPAP->days = (gPAP->days & 0xff00) | tmpbyte;
 				gPANS = 0xd600;
 			}
 			break;
-		case 7:		/* Update High Days */
-			if (gPANC & 0x2000){	/* Read */
+		case 7: /* Update High Days */
+			if (gPANC & 0x2000)
+			{ /* Read */
 				tmpbyte = (gPAP->days >> 8);
 				gPANS = 0xf700 | tmpbyte;
-			} else {		/*Write */
+			}
+			else
+			{ /*Write */
 				tmpbyte = gPANC & 0x00ff;
-				gPAP->days = (gPAP->days & 0x00ff) | ((ushort)tmpbyte)<<8;
+				gPAP->days = (gPAP->days & 0x00ff) | ((ushort)tmpbyte) << 8;
 				gPANS = 0xd700;
 			}
 			break;
-		default :	/* This should never happen */
+		default: /* This should never happen */
 			break;
 		}
-		if (debug) fprintf(debugfile,"panel_event: TRR - result: gPANS = %0x04\n",gPANS);
-		if (debug) fflush(debugfile);
+		if (debug)
+			fprintf(debugfile, "panel_event: TRR - result: gPANS = %0x04\n", gPANS);
+		if (debug)
+			fflush(debugfile);
 	}
-	if (gPAP->sec_tick) {	/* Seconds tick from rtc, update counters */
-		//if (debug) fprintf(debugfile,"panel_event: 1 second tick\n");
-		//if (debug) fflush(debugfile);
+	if (gPAP->sec_tick)
+	{ /* Seconds tick from rtc, update counters */
+		// if (debug) fprintf(debugfile,"panel_event: 1 second tick\n");
+		// if (debug) fflush(debugfile);
 		gPAP->sec_tick = false;
 		gPAP->seconds++;
-		if (gPAP->seconds >= 43200){	/* 12h wraparound */
+		if (gPAP->seconds >= 43200)
+		{ /* 12h wraparound */
 			gPAP->seconds = 0;
 			gPAP->days++;
 		}
 	}
-
 }
 
-void panel_processor_thread() {
+void panel_processor_thread()
+{
 	int s;
-	while(CurrentCPURunMode != SHUTDOWN) {
+	while (CurrentCPURunMode != SHUTDOWN)
+	{
 		while ((s = sem_wait(&sem_pap)) == -1 && errno == EINTR) /* wait for pap 'kick' */
-			continue; /* Restart if interrupted by handler */
+			continue;											 /* Restart if interrupted by handler */
 
 		panel_event();
 	}
--- iox/panel.h
+++ iox/panel.h
@@ -4,50 +4,144 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "../nd100.h"
 
+// More doumentaion here:
+// https://www.ndwiki.org/wiki/ND-100_front_panel
+// http://sintran.com/sintran/hardware/nd-other/nd-322691.html
 
+/// @brief Message control functions.
+/// Read from WPAN bits 0-2
+/// Used when PANS/PANC PANEL_STATUS_FUNCTIONS is STATUS_MESSAGE_CONTROL
+typedef enum
+{
+	/// <summary>
+	/// Stop rotating the message
+	/// </summary>
+	StopRotatingMessage = 0b000,
 
-/*
- */
-struct control_panel {
-	int lock_key;
-//	bool stop_button;
-//	bool load_button;
-//	bool opcom_button;
-//	bool mcl_button;
+	/// <summary>
+	/// Return display to normal funciont
+	/// </summary>
+	ReturnDisplayToNormal = 0b001,
 
-	bool power_lamp;
-	bool run_lamp;
-	bool opcom_lamp;
-};
+	/// <summary>
+	/// Clear text buffer and Function display. Prepare for text to be appended
+	/// </summary>
+	ClearTextBuffer = 0b010,
 
+	/// <summary>
+	/// Rotate the message in the text buffer, displaying four characters at a a time
+	/// </summary>
+	RotateMessage = 0b100,
 
+	/// <summary>
+	/// CLear the text and start rotating. (Command 010 plus command 100)
+	/// </summary>
+	ClearAndRotate = 0b110,
+} MessageControl;
 
-struct display_panel {
-//	bool opcom_button;
+/// @brief User/external functions.
+/// Used when PANS/PANC bit 11 is set to 0
+typedef enum
+{
+	STATUS_ILLEGAL,
+	STATUS_FUTURE_EXTENSION,
+	STATUS_MESSAGE_APPEND,
+	STATUS_MESSAGE_CONTROL,
+	STATUS_UPDATE_LOW_SECONDS,
+	STATUS_UPDATE_HIGH_SECONDS,
+	STATUS_UPDATE_LOW_DAYS,
+	STATUS_UPDATE_HIGH_DAYS,
 
-	bool trr_panc;	/* TRR has been issued, process command */
-	bool sec_tick;	/* Seconds tick from rtc, update counters */
+	// ND INTERNAL FUNCTIONS - SEE ND-110 MICROCODE
+	STATUS_MEMORY_EXAMINE,	 // 8 - After 2 bytes from AB,OCTA2
+	STATUS_DATA_TO_EXAMINE,	 // 9 - After 2 bytes with register P (DYTP1:)
+	STATUS_ACTIVE_LEVELS,	 // 10 - After 2 bytes with register Q  (DYTP2:)
+	STATUS_UNKNOWN_11,		 // 11 -
+	STATUS_UNKNOWN_12,		 // 12 -
+	STATUS_OUT_EXAMINE_MODE, // 13 - (EXM02) after 1 byte Q ?
+	STATUS_SEND_LABEL,		 // 14 - After, 2 words: AB,TXT1 then AB,TXT2 (LABEL:)
+	STATUS_F_TYPED			 // 15 -
+} PANEL_STATUS_FUNCTIONS;
 
-	uint16_t pap_curr_command;
+///  PANS (READ)
+///  Response from panel to ND-100 cpu
+///
+/// +-----------+--------+----------+-------+------------+------------+
+/// |	15      |  14    |  13		|   12  | 11-10-9-8  | 7    -   0 |
+/// +-----------+--------+----------+-------+------------+------------+
+/// |DISP.PRESS |INP PDY | RPAN VAL |PAN INT|   PFUNC    |   RPAN     |
+/// +-----------+--------+----------+-------+------------+------------+
+typedef union
+{
+	uint16_t raw;
+	struct
+	{
+		uint16_t rpan : 8;				  // Bits 0-7: RPAN (return from Panel)
+		PANEL_STATUS_FUNCTIONS pfunc : 4; // Bits 8-10: PFUNC (Panel function code returned))
+		uint16_t pan_interrupt : 1;		  // Bit 12: Panel Interrupt (set to 1 when responding) - Also known as "COM RDY", The command in PCOM has been processed
+		uint16_t read_panel_valid : 1;	  // Bit 13: RPAN Valid (If 1, then last command PAP processed was a READ, data is in bit 0-7)
+		uint16_t input_pending : 1;		  // Bit 14: Input Pending (When 0, the PAC fifo is full. Is status stays at 0 for more than 2 ms then PAP (Panel Processor) is not working)
+		uint16_t panel_present : 1;		  // Bit 15: Panel Pressent (1=Display is present)
+	} bits;
+} PANS_Register;
 
-	char func_display[40];	/* Max 40 chars in display buffer */
-	uint16_t fdisp_cntr;	/* pointer to where we are */
+///  PANC - Panel Control Register (WRITE)
+///  Command from ND-100 cpu to panel
+///
+/// +---+---+--------------+----+-----------+------------+
+/// |15 | 14|  13		   | 12 | 11-10-9-8 | 7    -   0 |
+/// +---+---+--------------+----+-----------+------------+
+/// | 0 | 0 | Read Request |N.A.|  PFUNC    |   WPAN     |
+/// +---+---+--------------+----+-----------+------------+
+typedef union
+{
+	uint16_t raw;
+	struct
+	{
+		uint16_t wpan : 8;				  // Bits 07: WPAN (Write to Panel)
+		PANEL_STATUS_FUNCTIONS pfunc : 4; // Bits 8-11: PFUNC (Panel function code)
+		uint16_t reserved12 : 1;		  // Bit 12: N.A.
+		uint16_t read_request : 1;		  // Bit 13: Read Request
+		uint16_t reserved14 : 1;		  // Bits 14: Reserved
+		uint16_t reserved15 : 1;		  // Bits 15: Reserved
+	} bits;
+} PANC_Register;
 
-	uint16_t seconds;		/* 16 bit second counter for realtime clock, ticks day counter every 12h */
-	uint16_t days;		/* 16 bit day counter for realtime clock */
-				/* So seconds should wrap at 3600x12 = 43200, and day possibly lowest bit is am/pm */
+struct display_panel
+{
+	bool sec_tick; /* Seconds tick from rtc, update counters */
 
-	bool power_lamp;
-	bool run_lamp;
-	bool opcom_lamp;
-	int function_util;
-	int function_hit;
-	int function_ring;
-	int function_mode;
+	char func_display[40]; /* Max 40 chars in display buffer */
+	uint16_t fdisp_cntr;   /* pointer to where we are */
+
+	uint16_t seconds; /* 16 bit second counter for realtime clock, ticks day counter every 12h */
+	uint16_t days;	  /* 16 bit day counter for realtime clock */
+					  /* So seconds should wrap at 3600x12 = 43200, and day possibly lowest bit is am/pm */
+
+	// bool power_lamp; 
+	// bool run_lamp; // read from RUN signal on ND-100 bus C. Here, maybe CPU in RUN mode?
+	// bool opcom_lamp; // read from signal on ND-100 bus C. Her maybe CPU in STOP mode ?
+
+	//  int lock_key; // Is the key in LOCK, ON or STANDBY position
+	//	bool stop_button;
+	//	bool load_button;
+	//	bool opcom_button;
+	//	bool mcl_button;
+
+	int utilization;   // Utilization percentage (calculated based on PIL level on 0 versus other PIL levels.0 =IDLE)
+	int function_hit;  // Cache hit ? maybe on ND-BUS C signal
+	int function_ring; // PIL register, or 1<<PIL to show which PIL bit is set
+	int function_mode; // Panel Processor Functtion Mode Register (not used yet..)
 };
 
 struct display_panel *gPAP;
+extern struct CpuRegs *gReg; // cpu.c
 
+void ProcessTerminalPanc();
+void ProcessTerminalLamp();
+void UpdateMachineTime();
+void setup_pap();
 
 #endif // PANEL_H
